90b0d2d6d45e6fb8ac913a56e4da84a040442be9
Quetzal <quetzaluzcategui@gmail.com>
2024-06-03 08:26:38 -0400

    Initial Commit

---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Quetzal on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
